7a4f4fda89dd6bfb2285aa94817bcc4b72c73746
Jordan <jordancdiaz04@gmail.com>
2023-09-17 02:45:09 -0400

    added arrow and descriptions

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,17 +3,22 @@ import "./App.css";
 import AddressInput from "./components/AddressInput.js";
 import Header from "./components/Header.js";
 import Hero from "./components/Hero.js";
+import markerArrow from "./assets/markerarrow.png"
+import Descriptions from "./components/Descriptions.js";
 
 
 function App() {
-
-
 
   return (
     <>
       <Header />
       <Hero />
       <AddressInput />
+      <div className="line-container">
+        <img src={markerArrow} id="marker-arrow"/>
+      </div>
+      <Descriptions />
+      
     </> 
   );
 }
--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -6,7 +6,6 @@ function Header(){
     return (
         <div className="header">
         <img src={solar} id="logo" />
-        {/* <h2>Save your planet and your wallet</h2> */}
           <div className="btn-container">
             <NavButton text={"Register"} />
             <NavButton text={"Dashboard"} />
